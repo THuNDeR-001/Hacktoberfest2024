5550bf047301183b5730210d4048a9ab29321768
THuNDeR-001 <128207888+THuNDeR-001@users.noreply.github.com>
2024-10-15 16:53:42 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Harshvardhan Mittal",
+    username; "https://github.com/THuNDeR-001",
+  },
   
 ];
